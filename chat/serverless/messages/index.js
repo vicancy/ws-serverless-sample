114d1bf259bd31fdc6fd7e8e71b3d39a7bc810f5
Liangying Wei <lianwei@microsoft.com>
2019-11-18 19:09:48 +0800

    Add chat.html for ws and improve serverless side

--- chat/serverless/messages/index.js
+++ chat/serverless/messages/index.js
@@ -1,3 +1,5 @@
+var http = require('http');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log(req.body);
@@ -11,19 +13,60 @@ module.exports = async function (context, req) {
 
     if (event === "connect") {
         // do auth?
+        context.res = {
+            body: user + "(" + connectionId + ") logged in."
+        }
         
     } else if (event === "disconnect") {
+        // todo: integrate with Azure Storage
+        console.log(user + "(" + connectionId + ") disconnected");
         // clean up
-    }
-    if (req.body) {
-        context.res = {
-          body: "Hello" + req.body  
-        };
+    } else if (event == "message"){
+        if (req.body) {
+            var response = '';
+            var message = req.body;
+            if (message.group){
+                if (message.action){
+                    const recipient = message.recipient || "self";
+                    if (message.action === "add"){
+                        // TODO: add to group
+                        response = "Adding " + recipient + " to group " + message.group;
+                        console.log(response);
+                    } else if (message.action === "remove"){
+                        // TODO; remove from group
+                        response = "Removing " + recipient + " from group " + message.group;
+                        console.log(response);
+                    }
+                } else{
+                    // TODO: send to group
+                    response = "Sending to group " + message.group + ": " + message.text;
+                }
+            } else {
+                if (message.action){
+                    if (message.action === "broadcast") {
+                        // TODO: broadcast
+                        response = "Broadcast: " + message.text;
+                    }
+                }
+                else {
+                    // TODO: send to user
+                    response = "Send to user " + message.recipient + ": " + message.text;
+                }
+            }
+            context.res = {
+              body: response,
+            };
+        } else {
+            context.res = {
+                status: 400,
+                body: "Invalid message body."
+            }
+        }
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Invalid event."
         };
     }
 };
---
+++ chat/serverless/negotiate/index.js
@@ -0,0 +1,47 @@
+
+const connectionString = 'Endpoint=http://localhost;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;Port=8080;Version=1.0;';
+const key = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const audience = 'http://localhost/ws/client/';
+const url = 'ws://localhost:8080/ws/client/?hub=chat&serverless=true';
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+var func = module.exports = async function (context, req) {
+    if (req.query.name || (req.body && req.body.name)) {
+        const user = (req.query.name || req.body.name);
+        const options = {
+            issuer:  'chat',
+            subject: user,
+            audience:  audience,
+            expiresIn:  "12h",
+            algorithm:  "HS256"
+        };
+    
+        const payload = {
+            "user" : user,
+        };
+
+        const token = jwt.sign(payload, key, options);
+        const clientUrl = url + "&access_token=" + token;
+        context.res = {
+            body: {
+                url : clientUrl
+            }
+        };
+    }
+    else {
+        context.res = {
+            status: 401,
+        };
+    }
+};
+
+
+var context = {
+    res : {body: ""}
+};
+var req = { 
+    query: {name : "hello"}
+};
+func(context, req);
+console.debug(context.res.body);
